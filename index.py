11334ea8eca803d3b160eb8a0b224c4ba67a0fd5
Ilya <72245457+terrorChay@users.noreply.github.com>
2022-10-26 21:58:34 +0300

    commit stal_krutim

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ def run_query(query):
 # Настройка
 def init():
     # Настройка страницы
-    st.set_page_config(layout='wide', page_title='Ilya Matyushin')
+    st.set_page_config(layout='wide', page_title='Ilya Matyushin 1337')
 
     # Подгрузка бибилотеки цветных градиентов
     global colorscales
