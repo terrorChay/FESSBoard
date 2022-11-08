ed417e3e2e96f84115ca31e47cfb88d195f2e0ad
Ilya <72245457+terrorChay@users.noreply.github.com>
2022-11-09 02:11:20 +0300

    Update 03_🤝_Яумер.py

--- streamlit_app/pages/03_🤝_Яумер.py
+++ streamlit_app/pages/03_🤝_Яумер.py
@@ -14,14 +14,14 @@ def main():
     w = SimpleNamespace(
         dashboard=board,
         # Parameters: element_identifier, x_pos, y_pos, width, height, static, isDraggable, isResizable, resizeHandles
-        pie=Pie(board, 6, 0, 6, 8, minW=3, minH=4),
+        pie=Pie(board, 6, 0, 7, 8, minW=3, minH=4),
         card=Card(board, 0, 0, 3, 8, minW=2, minH=4, isResizable=False)
     )
     with elements("kek"):
-        with w.dashboard(rowHeight=57):
+        with w.dashboard(cols={'lg': 10, 'md': 10, 'sm': 6, 'xs': 4, 'xxs': 2}, rowHeight=57):
             w.pie('')
             w.card("Илья умер в попытках познать Material UI. Его спас добрый самаритянин, который интегрировал его со стримлитом и разместил на гитхабе примеры.")
-
+    st.title('lol kek cheburek')
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
     main()
