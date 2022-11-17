7dfd90a99d644647caea97bf8212164be4500768
Ilya <72245457+terrorChay@users.noreply.github.com>
2022-11-17 18:09:09 +0300

    Update Dashboard.py

--- streamlit_app/Dashboard.py
+++ streamlit_app/Dashboard.py
@@ -1,9 +1,18 @@
 import streamlit as st
 from streamlit import session_state as session
-from st_init import init
 import pandas as pd
 import plotly.express as px
 
+def init():
+    # Настройки страницы
+    st.set_page_config(layout='wide', page_title='FESSBoard')
+    # Импорт CSS стилей
+    try: # Local launch
+        with open('/app/fessboard/streamlit_app/styles.css', 'r') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    except FileNotFoundError: # Streamlit Cloud
+        with open('styles.css', 'r') as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def main():
     # Настройка страницы
