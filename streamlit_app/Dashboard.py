36855492f7bedf796617fc226d74e08f78810698
yourmasterm1nd <116177845+yourmasterm1nd@users.noreply.github.com>
2022-11-17 18:00:53 +0300

    Update Dashboard.py

--- streamlit_app/Dashboard.py
+++ streamlit_app/Dashboard.py
@@ -2,6 +2,39 @@ import streamlit as st
 from streamlit import session_state as session
 from st_init import init
 
+
+
+def main():
+    # Настройка страницы
+    st.set_page_config(
+        page_title="Главная Страница",
+        page_icon="✋🏻" 
+    )
+    from connectdb import conn
+
+    st.title("Главная Страница")
+    st.sidebar.success("Выберете страницу 📖")
+
+    # Подгрузка бибилотеки цветных градиентов
+    global colorscales
+    colorscales = px.colors.named_colorscales()
+
+    frame = pd.read_sql('select * from projects', conn)
+    st.dataframe(pd.DataFrame(frame))
+
+    df = frame
+
+    fig = px.pie(df.loc[df['project_company'] > 5], values = 'project_company', names = 'project_name')
+    st.write (fig)
+
+    fig_1 = px.pie(df, values = 'project_id', names = 'project_field', hole =.2)
+    st.write (fig_1)
+
+    st.bar_chart(df, x = 'project_end_date', y = 'project_id')
+
+
+
+
 # Подключение к БД
 def run():
     test = st.text_input('Переменная на странице дашборд')
@@ -12,3 +45,4 @@ def run():
 if __name__ == '__main__':
     init()
     run()
+    main()
